1b5110e7c1b257257d86e44855376d7f0d28ba56
RayMuir <rm717@kent.ac.uk>
2025-12-02 12:18:36 +0000

    Day 1

---
+++ Day1P1.py
@@ -0,0 +1,21 @@
+x = open("day1_input1.txt", "r")
+
+data = x.read()
+data_list = data.split("\n")
+
+
+current_point = 50
+zeroes = 0
+
+for move in data_list:
+    number = int(move[1:])
+    if move[0] == "R":
+        current_point = (current_point + number)
+    elif move[0] == "L":
+        current_point = (current_point - number)
+    else:
+        print("Fail")
+    current_point = current_point % 100
+    if current_point == 0:
+        zeroes += 1
+print(zeroes)
