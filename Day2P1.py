916e7e6de022ada1364f9160f562d05eeff6dc53
RayMuir <rm717@kent.ac.uk>
2025-12-02 18:46:15 +0000

    Part 1 answer

---
+++ Day2P1.py
@@ -0,0 +1,36 @@
+x = open("day2_input.txt", "r")
+
+data = x.read()
+data_list = data.split(",")
+
+new_list = []
+for x in data_list:
+    y = x.split("-")
+    new_list.extend(y)
+
+total = 0
+
+# new_list = [11, 22, 95, 115, 998, 1012, 1188511880, 1188511890, 222220, 222224, 1698522, 1698528, 446443, 446449, 38593856, 38593862]
+
+y = len(new_list)
+if y % 2 == 0:
+    y = y // 2
+else:
+    y = (y // 2) + 1
+
+for _ in range(y):
+    smaller = int(new_list.pop(0))
+    bigger = int(new_list.pop(0))
+
+    amt = (bigger - smaller) + 1
+
+    for n in range(amt):
+        number = smaller + n
+        current = str(number)
+        if len(current) % 2 == 0:
+            fst_half = current[ : (len(current) // 2)]
+            snd_half = current[(len(current) // 2) : ]
+            if fst_half == snd_half:
+                total += number
+
+print(total)
