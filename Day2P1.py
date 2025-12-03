9f4419ee3f2c0927d285d7b9f5227287b31b7116
RayMuir <rm717@kent.ac.uk>
2025-12-03 17:46:22 +0000

    day2 part 1 answer

--- Day2P1.py
+++ Day2P1.py
@@ -12,20 +12,12 @@ total = 0
 
 # new_list = [11, 22, 95, 115, 998, 1012, 1188511880, 1188511890, 222220, 222224, 1698522, 1698528, 446443, 446449, 38593856, 38593862]
 
-y = len(new_list)
-if y % 2 == 0:
-    y = y // 2
-else:
-    y = (y // 2) + 1
-
+y = (len(new_list) + 1) // 2
 for _ in range(y):
     smaller = int(new_list.pop(0))
     bigger = int(new_list.pop(0))
 
-    amt = (bigger - smaller) + 1
-
-    for n in range(amt):
-        number = smaller + n
+    for number in range(smaller, bigger + 1):
         current = str(number)
         if len(current) % 2 == 0:
             fst_half = current[ : (len(current) // 2)]
@@ -34,3 +26,20 @@ for _ in range(y):
                 total += number
 
 print(total)
+
+
+# y = (len(new_list) + 1) // 2
+
+# for smaller, bigger in zip(new_list[::2], new_list[1::2]):
+#     smaller = int(smaller)
+#     bigger = int(bigger)
+
+#     for number in range(smaller, bigger + 1):
+#         current = str(number)
+#         if len(current) % 2 == 0:
+#             fst_half = current[ : (len(current) // 2)]
+#             snd_half = current[(len(current) // 2) : ]
+#             if fst_half == snd_half:
+#                 total += number
+
+# print(total)
