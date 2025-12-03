6b1dfd8d344041f5d25d16a72db83b57597f9221
RayMuir <rm717@kent.ac.uk>
2025-12-03 17:46:48 +0000

    day 2 part 2 answer

--- Day2P2.py
+++ Day2P2.py
@@ -10,21 +10,29 @@ for x in data_list:
 
 total = 0
 
-# new_list = [11, 22, 95, 115, 998, 1012, 1188511880, 1188511890, 222220, 222224, 1698522, 1698528, 446443, 446449, 38593856, 38593862]
+# new_list = [11, 22, 95, 115, 998, 1012, 1188511880, 1188511890, 222220, 222224, 1698522, 1698528, 446443, 446449, 38593856, 38593862, 565653, 565659, 824824821, 824824827, 2121212118, 2121212124]
 
-y = len(new_list)
-if y % 2 == 0:
-    y = y // 2
-else:
-    y = (y // 2) + 1
+y = (len(new_list) + 1) // 2 # ceiling div
 
 # For every pair of numbers in the data
 for _ in range(y):
     smaller = int(new_list.pop(0))
     bigger = int(new_list.pop(0))
 
-    amt = (bigger - smaller) + 1
     # For each number in the range of the pairs.
-    for n in range(amt):
-        number = smaller + n
+    for number in range(smaller, bigger + 1):
         current = str(number)
+ 
+        # we want to check up to half way for patterns
+        up_to_multiple = len(current) // 2
+        
+        for n in range(1, up_to_multiple + 1):
+            # is n a multiple of the current number
+            if len(current) % n == 0: 
+                pattern = current[:n]
+                multiplier = len(current) // n
+                test = pattern * multiplier
+                if test == current:
+                    total += number
+                    break
+print(total)
